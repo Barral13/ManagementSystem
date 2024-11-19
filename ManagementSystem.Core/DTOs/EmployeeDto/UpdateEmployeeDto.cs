43d76f2b02d99fb8bc46e746418a88245358cf7d
Dhanglerson Barral <dhanglerson.barral@crfsp.org.br>
2024-11-19 11:18:11 -0300

    Create Controllers and Services

--- ManagementSystem.Core/DTOs/EmployeeDto/UpdateEmployeeDto.cs
+++ ManagementSystem.Core/DTOs/EmployeeDto/UpdateEmployeeDto.cs
@@ -2,13 +2,13 @@
 
 public class UpdateEmployeeDto
 {
-    public string? FirstName { get; set; } = string.Empty;
-    public string? LastName { get; set; } = string.Empty;
-    public string? Email { get; set; } = string.Empty;
-    public string? PhoneNumber { get; set; } = string.Empty;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
     public decimal? Salary { get; set; }
 
     public int? DepartmentId { get; set; }
     public int? PositionId { get; set; }
-    public DateTime UpdatetedAt { get; set; } = DateTime.Now;
+    public DateTime UpdatedAt { get; set; } = DateTime.Now;
 }
